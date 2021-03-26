9b0b3e7834502abac7ebbbed509062220193e2be
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-03-26 23:35:28 +0700

    Update soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-tanggal="`date`"
+tanggal="`date +%d-%m-%Y`"
 
 for ((i=1; i<=23; i=i+1))
 do
 if [ $i -lt 10 ]
 then
-wget -P ~/"$tanggal" -O Koleksi_0${i} https://loremflickr.com/320/240/kitten &>> ~/"$tanggal"/Foto.log
+mv Koleksi_0${i} "$tanggal"
 else
-wget -P ~/"$tanggal" -O Koleksi_${i} https://loremflickr.com/320/240/kitten &>> ~/"$tanggal"/Foto.log
+mv Koleksi_${i} "$tanggal"
 fi
 done
+mv Foto.log "$tanggal"
