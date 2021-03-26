b510d4ec9bda07baed286fb30660a918040bb548
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-03-26 23:01:07 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
+tanggal="`date`"
+
 for ((i=1; i<=23; i=i+1))
 do
 if [ $i -lt 10 ]
 then
-wget -P /home/dewanggad99/kitten -O Koleksi_0${i} https://loremflickr.com/320/240/kitten &>> /home/dewanggad99/kitten/testtest.txt
+wget -P ~/"$tanggal" -O Koleksi_0${i} https://loremflickr.com/320/240/kitten &>> ~/"$tanggal"/Foto.log
 else
-wget -P ~/kitten -O Koleksi_${i} https://loremflickr.com/320/240/kitten &>> ~/kitten/testtest.txt
+wget -P ~/"$tanggal" -O Koleksi_${i} https://loremflickr.com/320/240/kitten &>> ~/"$tanggal"/Foto.log
 fi
-for ((j=1; j<i; j=j+1))
-do
-if [ $j -lt 10 ]
-then
-if
-done
 done
 
 
