3e6db3d94596468ff6c47ff4f1114eeef6230fb3
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-03-26 23:15:16 +0700

    Update soal3a.sh

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-tanggal="`date`"
-
 for ((i=1; i<=23; i=i+1))
 do
 if [ $i -lt 10 ]
 then
-wget -P ~/"$tanggal" -O Koleksi_0${i} https://loremflickr.com/320/240/kitten &>> ~/"$tanggal"/Foto.log
+wget -O Koleksi_0${i} https://loremflickr.com/320/240/kitten &>> Foto.log
 else
-wget -P ~/"$tanggal" -O Koleksi_${i} https://loremflickr.com/320/240/kitten &>> ~/"$tanggal"/Foto.log
+wget -O Koleksi_${i} https://loremflickr.com/320/240/kitten &>> Foto.log
 fi
 done
 
