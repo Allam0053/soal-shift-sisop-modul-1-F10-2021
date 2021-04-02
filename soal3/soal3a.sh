29d65e5247e65cd7a556d635f0e1ddd824512a03
rizadwiandhika <rizadwiandhika@gmail.com>
2021-04-02 17:12:39 +0700

    3a finished

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -2,6 +2,48 @@
 
 TOTAL_FOTO=23
 
+formatNamaFile() {
+  nomorDihapus=$1
+  for ((i=$nomorDihapus; i<$TOTAL_FOTO; i=i+1)); do
+    lebihSatu=$(($i+1))
+
+    namaBaru="Koleksi_$i"
+    namaLama="Koleksi_$lebihSatu"
+
+    if [ $i -lt 10 ]; then namaBaru="Koleksi_0$i"; fi
+    if [ $lebihSatu -lt 10 ]; then namaLama="Koleksi_0$lebihSatu"; fi
+
+    mv $namaLama $namaBaru
+  done
+}
+
+for ((num=1; num<=$TOTAL_FOTO; num=num+1)); do
+  namaFile="Koleksi_$num"
+  if [ $num -lt 10 ]; then namaFile="Koleksi_0$num"; fi
+  curl -Lo ./$namaFile -k https://loremflickr.com/320/240/kitten 2>> Foto.log
+done
+
+sama=0
+for ((i=1; i<=$TOTAL_FOTO; i=i+1)); do
+  for ((j=1; j<=$TOTAL_FOTO; j=j+1)); do
+    file1="Koleksi_$i"
+    file2="Koleksi_$j"
+
+    if [ $i -lt 10 ]; then file1="Koleksi_0$i"; fi
+    if [ $j -lt 10 ]; then file1="Koleksi_0$j"; fi
+    if [ $i == $j -o ! -f $file1 -o ! -f $file2 ]; then continue; fi
+    
+    fileSama=$(cmp $file1 $file2)
+    if [ -z $fileSama ]; then 
+      sama=$(($sama+1))
+      rm $file2
+      formatNamaFile $j
+    fi
+  done
+done
+
+# echo "ada $sama file yang sama"
+
 # Operasi matematika
 # totalIterasi=1
 # totalIterasi=$(($totalIterasi + 1))
@@ -10,15 +52,6 @@ TOTAL_FOTO=23
 # namaDepan="Riza"
 # namaBelakang="Andhika"
 # nama="$namaDepan $namaBelakang"
-
-for ((num=1; num<=23; num=num+1))
-do
-  namaFile="Koleksi_$num"
-  if [ $num -lt 10 ]; then
-    namaFile="Koleksi_0$num"
-  fi
-  curl -Lo./$namaFile -k https://loremflickr.com/320/240/kitten 2>> Foto.log
-done
 
 # for ((i=1; i<=23; i=i+1))
 # do
